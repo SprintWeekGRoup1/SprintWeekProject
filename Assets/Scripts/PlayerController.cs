d5741e357af5e9d0b845d5e26a24d3676f7b3a12
Emil Kola <emil.kola@gmail.com>
2023-11-11 14:21:19 +0200

    Added "Coyote Time" to the Jumping controls.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform detectGround;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject pauseObj;
+    [SerializeField] private float coyoteTime;
+
     public float Speed = 10f;
     public float JumpingPower = 10f;
-
-    private bool isFacingRight = true;
-    private bool pauseActive;
 
     private Rigidbody2D rb;
     private Vector2 movement = Vector2.zero;
 
-    [SerializeField] private Transform detectGround;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private GameObject pauseObj;
-
+    private bool isFacingRight = true;
+    private bool pauseActive;
+    private bool isGrounded;
+    private float coyoteTimeCounter = 0f;
 
     private void Start()
     {
@@ -32,6 +36,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Flip();
+        CoyoteTime();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -41,7 +46,9 @@ public class PlayerController : MonoBehaviour
 
     public void PlayerJump()
     {
-        if (isGrounded())
+        isGrounded = GroundedCheck();
+
+        if (isGrounded || coyoteTimeCounter > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
         }
@@ -72,10 +79,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool isGrounded()
+    private bool GroundedCheck()
     {
         // this create a check to detect if the player is on the ground in order to allow the player to jump
         return Physics2D.OverlapCircle(detectGround.position, 0.2f, groundLayer);
     }
 
+    // Let's the player jump after not touching ground for a a brief grace period
+    private void CoyoteTime()
+    {
+        if (!isGrounded)
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+    }
 }
