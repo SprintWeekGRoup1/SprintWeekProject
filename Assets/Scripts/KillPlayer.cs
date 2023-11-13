f672e879e24d06b98b8b0f71cc178ac93cb0e525
Emil Kola <emil.kola@gmail.com>
2023-11-13 16:23:29 +0200

    Reseted player position, removed killplayer script from player.

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
-  
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
